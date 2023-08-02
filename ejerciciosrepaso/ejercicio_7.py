f88d06a723fc2cec5e0e2804bc48c7c9d40cb4eb
NattVR <nathaliav8194@gmail.com>
2023-08-01 20:59:41 -0500

    Add ejercicio 7

---
+++ ejerciciosrepaso/ejercicio_7.py
@@ -0,0 +1,6 @@
+"""Escribir un programa que encuentre el número más grande y el más pequeño en una lista dada."""
+lista = [6,8,9,12,23,45,56,78,4,90,123]
+
+menor = min(lista)
+mayor = max(lista)
+print(f"El menor es {menor},y el mayor es {mayor}")
