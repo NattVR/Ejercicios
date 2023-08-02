5d903ac7f91802e9ee6c3c04eff59bba97e25d96
NattVR <nathaliav8194@gmail.com>
2023-08-01 20:08:44 -0500

    Add ejercicio 1

---
+++ ejerciciosrepaso/ejercicio_1.py
@@ -0,0 +1,6 @@
+"""Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla"""
+
+nombre_completo=input("Ingrese su nombre completo:")
+edad=int(input("Ingrese su edad"))
+print(nombre_completo)
+print(edad)
