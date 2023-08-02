3a265d7f08f49a3b6052e385d9c8f1471ec36585
NattVR <nathaliav8194@gmail.com>
2023-08-01 20:18:46 -0500

    Add ejercicio 3

---
+++ ejerciciosrepaso/ejercicio_3.py
@@ -0,0 +1,5 @@
+"""Crear un programa que genere una lista de números aleatorios y los imprima en pantalla"""
+import random
+for i in range(1, 10):
+    numero_aleatorio = random.randint(1, 20)
+    print(numero_aleatorio)
