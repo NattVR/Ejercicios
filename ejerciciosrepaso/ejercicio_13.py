bcd61d5ea336335f3bec2f50c06967247c7ad1a4
NattVR <nathaliav8194@gmail.com>
2023-08-01 21:01:36 -0500

    Add ejercicio 13

---
+++ ejerciciosrepaso/ejercicio_13.py
@@ -0,0 +1,10 @@
+"""Crear un programa que pida al usuario ingresar dos números y calcule su suma, resta, multiplicación y
+división"""
+numero_1 = float(input("Ingrese un número:"))
+numero_2 = float(input("Ingrese otro número:"))
+
+suma = numero_1+numero_2
+resta = numero_1-numero_2
+multiplicacion = numero_1*numero_2
+division = numero_1/numero_2
+print(f"La suma es {suma}\nLa resta es {resta}\nLa multiplicacion es {multiplicacion}\nLa division es {division}")
